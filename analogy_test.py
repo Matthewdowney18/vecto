5a34ab8f4c8e38db7038d64a8ee3ff0249e54b2d
Matthew Downey <matthew_downey@student.uml.edu>
2018-07-20 16:40:05 -0400

    second save

--- analogy_test.py
+++ analogy_test.py
@@ -8,12 +8,17 @@ from vecto.benchmarks.analogy import LRCos
 
 
 class embeddings:
-    def __init__(self, i):
-        self.embedding = i.split('/')[5]
+    name = 0
+    def __init__(self, dict):
+        i = dict['path']
+        self.embedding_name = i.split('/')[5]
         self.embedding_directory = i
         self.embeddings = vecto.embeddings.load_from_dir(self.embedding_directory)
+        self.citation = dict['citation']
+        self.description = dict['description']
 
     def get_analogy(self, dataset):
+        self.dataset_path = dataset
         analogy = LRCos()
         self.result = analogy.get_result(self.embeddings, dataset)
         save_json(self.result, "./res.json")
@@ -24,38 +29,25 @@ class embeddings:
         for subcategory in self.subcategories:
             self.row[subcategory] = 1
 
-    def get_missing_pairs(self):
-        self.missing_pairs=[]
-        for i in results:
-            self.missing_pairs.append(i)
+    def get_dictionary(self):
+        dictionary = {}
+        dictionary['embedding'] = self.embedding_name
+        for i in self.result:
+            subcategory = i["experiment_setup"]["subcategory"]
+            results = i['result']
+            missing_pairs = self.get_length(subcategory) - i["experiment_setup"]["cnt_questions_total"]
+            dictionary[subcategory] = {'results' : results,'missing pairs' : missing_pairs }
+        dictionary['description'] = self.description
+        dictionary['citation'] = self.citation
+        print(dictionary)
+        return dictionary
 
-    def get_subcategory_results(self):
-        df = pandas.concat([self.json_to_df(j) for j in results])
-        df.to_csv('dataframe2.csv')
-        self.get_table(df)
-
-    def json_to_df(self, results):
-        meta = [["experiment_setup", "subcategory"], ["experiment_setup", "method"]]
-        df = json_normalize(results, record_path=["details"], meta=meta)
-        df["reciprocal_rank"] = 1 / (df["rank"] + 1)
-        df["embedding"] = self.embedding
-        df.to_csv('dataframe.csv')
-        return df
-
-    def get_table(self, df):
-        group = df.groupby(["experiment_setup.subcategory", "experiment_setup.method", "embedding"])
-        means = group.mean()
-        print(means)
-        means.to_csv('dataframe3.csv')
-        means.reset_index(inplace=True)
-        means = means.loc[:, ["experiment_setup.subcategory", "experiment_setup.method", "reciprocal_rank", "embedding"]]
-        unstacked = means.groupby(['experiment_setup.subcategory', 'experiment_setup.method'])[
-            'reciprocal_rank'].aggregate('first').unstack()
-        self.dataframe = means
-        unstacked.plot(kind="bar")
-        plt.show()
-
-
+    def get_length(self, filename):
+        i = 0
+        file = open(self.dataset_path+ filename, 'r')
+        for line in file:
+            i+=1
+        return i
 #embeddigns = vecto.embeddings.load_from_dir("/home/downey/PycharmProjects/jupyter_example/wiki.got(1)/")
 #embeddigns = vecto.embeddings.load_from_dir("/home/downey/PycharmProjects/jupyter_example/glove.twitter.27B/")
 #embeddigns = vecto.embeddings.load_from_dir("/home/downey/PycharmProjects/jupyter_example/!Demo2/embeddings/bnc/") #works
@@ -65,19 +57,27 @@ class embeddings:
 #embeddigns = vecto.embeddings.load_from_dir("/home/downey/PycharmProjects/jupyter_example/training-monolingual/")
 #embeddigns = vecto.embeddings.load_from_dir("/home/downey/PycharmProjects/jupyter_example/lstm/1/word/") #works
 
-directories = ["/home/downey/PycharmProjects/jupyter_example/!Demo2/embeddings/bnc/",
-               "/home/downey/PycharmProjects/jupyter_example/lstm/1/word/",
-               "/home/downey/PycharmProjects/jupyter_example/glove.6b/glove.6B/"]
+directories = [{'path':"/home/downey/PycharmProjects/jupyter_example/!Demo2/embeddings/bnc/",
+                'citation':'',
+                'description':''},
+               {'path':"/home/downey/PycharmProjects/jupyter_example/lstm/1/word/",
+                'citation':'',
+                'description':'subword model with LSTM composition function from Li et al. trained on text8 '},
+               {'path':"/home/downey/PycharmProjects/jupyter_example/glove.6b/glove.6B/",
+                'citation':'Jeffrey Pennington, Richard Socher, and Christopher D. Manning. 2014. GloVe: '
+                           'Global Vectors for Word Representation. [pdf] [bib] ',
+                'description':'Wikipedia 2014 + Gigaword 5 (6B tokens, 400K vocab, uncased, 50d, 100d, 200d, & 300d'
+                              ' vectors)'}]
 
 
 
 df = pandas.DataFrame()
-
+dicts = []
 for file in directories:
     embedding = embeddings(file)
-    results = embedding.get_analogy("/home/downey/PycharmProjects/jupyter_example/!Demo2/datasets/bats_small/")
-    embedding.get_subcategory_results()
-    df2 = embedding.dataframe.set_index("experiment_setup.subcategory")
-    df.append(df2)
-    df2.to_csv('dataframe5.csv')
-    print(df2.groupby('embedding', axis=1))
+    #results = embedding.get_analogy("/home/downey/PycharmProjects/jupyter_example/!Demo2/datasets/bats_small/")
+    results = embedding.get_analogy("/home/downey/PycharmProjects/jupyter_example/BATS_3.0/")
+    dicts.append(embedding.get_dictionary())
+    df = pandas.DataFrame(dicts).set_index('embedding')
+    df.to_csv('table1.csv')
+    print(df)
